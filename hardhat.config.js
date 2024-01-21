71f0f5428d57b9ef24d978e57bced80ebea75dd1
Swarup <banikswarup437@gmail.com>
2024-01-22 02:30:54 +0530

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
@@ -7,6 +8,10 @@ module.exports = {
     hardhat: {
       chainId: 1337,
     },
+    goerli: {
+      url: "process.env.GOERLI_URL",
+      accounts: [ `process.env.PRIVATE_KEY` ]
+    }
   },
   paths: {
     artifacts: "./client/src/artifacts",
